1df080f6dc23f4fd6c02f8aa2bb565c5ad3b31c6
jonne <jonnenc@gmail.com>
2024-09-19 20:30:38 -0400

    add styles for title and summary

--- src/components/NewsPanel.js
+++ src/components/NewsPanel.js
@@ -7,35 +7,29 @@ const NewsPanel = ({ image, title, summary}) => {
     };
 
     return (
-        <Pressable style={styles.button} onPress={handlePress}>
+        <Pressable style={styles.panel} onPress={handlePress}>
             <Image
                 source={{ uri: image }}
                 style={{ width: 200, height: 200 }}
             />
-            <Text style={styles.text}>{title}</Text>
-            <Text style={styles.text}>{summary}</Text>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.summary}>{summary}</Text>
         </Pressable>
     );
 };
 
 const styles = StyleSheet.create({
-    button: {
+    panel: {
         backgroundColor: "#38a169",
-        width: 240,
         paddingVertical: 10,
         paddingHorizontal: 50,
-        borderRadius: 7,
-        alignItems: "center",
-        justifyContent: "center",
     },
-    buttonText: {
-        fontWeight: "500",
-        color: "#ffffff",
-        fontSize: 18,
-        textAlign: "center",
+    title: {
+        fontSize: 20, // Equivalent to text-2xl
+        fontWeight: "semibold"
     },
-    text: {
-        fontSize: 12, // Equivalent to text-2xl
+    summary: {
+        fontSize: 12
     }
 });
 
--- src/screens/NewsAndEventsPage.js
+++ src/screens/NewsAndEventsPage.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import CustomButton from "../components/CustomButton";
 import NewsPanel from "../components/NewsPanel";
 
 export default function NewsAndEventsPage() {
@@ -9,7 +8,7 @@ export default function NewsAndEventsPage() {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>ASP.NET + React Native Demo Project</Text>
+            <Text style={styles.title}>News and Events</Text>
             <NewsPanel title = "Rohit is bad!!!!!!" summary = "He is not understanding what iim doing" image = "https://i.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=1280&format=png&auto=webp&s=7177756d1f393b6e093596d06e1ba539f723264b">
             </NewsPanel>
         </View>
@@ -29,10 +28,5 @@ const styles = StyleSheet.create({
         fontSize: 24, // Equivalent to text-2xl
         fontWeight: "bold",
         textAlign: "center",
-    },
-    buttonContainer: {
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 16, // Equivalent to mt-4
     },
 });

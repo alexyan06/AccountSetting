687cd85a0230edef66fd39c4227064e0fe801943
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-11-19 19:53:18 -0500

    Merge changes

--- App.js
+++ App.js
@@ -7,10 +7,6 @@ import HomePage from "./src/screens/HomePage";
 import NotFoundPage from "./src/screens/NotFoundPage";
 import MapPage from "./src/screens/MapPage";
 import NewsAndEventsPage from "./src/screens/NewsAndEventsPage";
-import LogIn from "./src/screens/LogIn";
-import Verification from "./src/screens/Verification";
-import IntroPage from "./src/screens/IntroPage";
-import Signup from "./src/screens/Signup";
 import SearchPage from "./src/screens/SearchPage";
 
 export default function App() {
@@ -19,20 +15,24 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="LogIn"
+        initialRouteName="Home"
         screenOptions={{
           headerShown: true, // Set to false to hide header
         }}
       >
-          <Stack.Screen name="IntroPage" component={IntroPage} />
-          <Stack.Screen name="LogIn" component={LogIn} />
-          <Stack.Screen name="Verification" component={Verification} />
-          <Stack.Screen name="Home" component={HomePage} />
-          <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="IntroPage" component={IntroPage} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+        <Stack.Screen name="Verification" component={Verification} />
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Map" component={MapPage} />
         <Stack.Screen name="NotFound" component={NotFoundPage} />
-          <Stack.Screen name="Search" component={SearchPage} />
-          <Stack.Screen name="NewsAndEventsPage" component={NewsAndEventsPage} />
+        <Stack.Screen name="Search" component={SearchPage} />
+        <Stack.Screen name="NewsAndEventsPage" component={NewsAndEventsPage} />
+        <Stack.Screen
+          name="IndividualNewsPage"
+          component={IndividualNewsPage}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );

97db9e9820edeb9f1d6b7fad91b086d6f6ae1a96
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-11-19 20:08:41 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,10 +5,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 // Pages
 import HomePage from "./src/screens/HomePage";
 import NotFoundPage from "./src/screens/NotFoundPage";
-import MapPage from "./src/screens/MapPage";
+// import MapPage from "./src/screens/MapPage";
 import NewsAndEventsPage from "./src/screens/NewsAndEventsPage";
 import SearchPage from "./src/screens/SearchPage";
 import IndividualNewsPage from "./src/screens/IndividualNewsPage";
+import IntroPage from "./src/screens/IntroPage";
+import LoginPage from "./src/screens/LoginPage";
+import VerificationPage from "./src/screens/VerificationPage";
+import SignupPage from "./src/screens/SignupPage";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -16,17 +20,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="LogIn"
         screenOptions={{
           headerShown: true, // Set to false to hide header
         }}
       >
         <Stack.Screen name="IntroPage" component={IntroPage} />
-        <Stack.Screen name="LogIn" component={LogIn} />
-        <Stack.Screen name="Verification" component={Verification} />
+        <Stack.Screen name="LogIn" component={LoginPage} />
+        <Stack.Screen name="Verification" component={VerificationPage} />
         <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Map" component={MapPage} />
+        <Stack.Screen name="Signup" component={SignupPage} />
+        {/* <Stack.Screen name="Map" component={MapPage} /> */}
         <Stack.Screen name="NotFound" component={NotFoundPage} />
         <Stack.Screen name="Search" component={SearchPage} />
         <Stack.Screen name="NewsAndEventsPage" component={NewsAndEventsPage} />
